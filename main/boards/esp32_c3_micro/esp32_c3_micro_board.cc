55458332eae478560b39f94fc877c78a23328d21
Nguyen Tiem <nguyentiemwork@gmail.com>
2026-06-07 20:06:13 +0700

    build with esp32 c3 micro custom board

---
+++ main/boards/esp32_c3_micro/esp32_c3_micro_board.cc
@@ -0,0 +1,162 @@
+#include "wifi_board.h"
+#include "codecs/no_audio_codec.h"
+#include "system_reset.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "mcp_server.h"
+#include "display/oled_display.h"
+#include "assets/lang_config.h"
+#include <esp_log.h>
+#include <driver/i2c_master.h>
+#include <esp_lcd_panel_ops.h>
+#include <esp_lcd_panel_vendor.h>
+#include <esp_lcd_io_i2c.h>
+#include "led/single_led.h"
+#include "settings.h"
+#include "power_save_timer.h"
+#include "press_to_talk_mcp_tool.h"
+#include <esp_efuse_table.h>
+
+
+#define TAG "ESP32C3MicroBoard"
+
+class ESP32C3MicroBoard : public WifiBoard {
+private:
+    Button touch_button_;
+    i2c_master_bus_handle_t display_i2c_bus_;
+    esp_lcd_panel_io_handle_t panel_io_ = nullptr;
+    esp_lcd_panel_handle_t panel_ = nullptr;
+    Display* display_ = nullptr;
+    PowerSaveTimer* power_save_timer_ = nullptr;
+    PressToTalkMcpTool* press_to_talk_tool_ = nullptr;
+
+    void InitializePowerSaveTimer() {
+        power_save_timer_ = new PowerSaveTimer(160, 300);
+        power_save_timer_->OnEnterSleepMode([this]() {
+            GetDisplay()->SetPowerSaveMode(true);
+        });
+        power_save_timer_->OnExitSleepMode([this]() {
+            GetDisplay()->SetPowerSaveMode(false);
+        });
+        power_save_timer_->SetEnabled(true);
+    }
+
+    void InitializeDisplayI2c() {
+        i2c_master_bus_config_t bus_config = {
+            .i2c_port = (i2c_port_t)0,
+            .sda_io_num = DISPLAY_SDA_PIN,
+            .scl_io_num = DISPLAY_SCL_PIN,
+            .clk_source = I2C_CLK_SRC_DEFAULT,
+            .glitch_ignore_cnt = 7,
+            .intr_priority = 0,
+            .trans_queue_depth = 0,
+            .flags = {
+                .enable_internal_pullup = 1,
+            },
+        };
+        ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &display_i2c_bus_));
+    }
+
+    void InitializeSsd1306Display() {
+        esp_lcd_panel_io_i2c_config_t io_config = {
+            .dev_addr = 0x3C,
+            .on_color_trans_done = nullptr,
+            .user_ctx = nullptr,
+            .control_phase_bytes = 1,
+            .dc_bit_offset = 6,
+            .lcd_cmd_bits = 8,
+            .lcd_param_bits = 8,
+
+            .flags = {
+                .dc_low_on_data = 0,
+                .disable_control_phase = 0,
+            },
+
+            .scl_speed_hz = 400 * 1000,
+        };
+
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(display_i2c_bus_, &io_config, &panel_io_));
+
+        ESP_LOGI(TAG, "Install SSD1306 driver");
+        esp_lcd_panel_dev_config_t panel_config = {};
+        panel_config.reset_gpio_num = -1;
+        panel_config.bits_per_pixel = 1;
+
+        esp_lcd_panel_ssd1306_config_t ssd1306_config = {
+            .height = static_cast<uint8_t>(DISPLAY_HEIGHT),
+        };
+        panel_config.vendor_config = &ssd1306_config;
+
+        ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(panel_io_, &panel_config, &panel_));
+        ESP_LOGI(TAG, "SSD1306 driver installed");
+
+        ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_));
+        if (esp_lcd_panel_init(panel_) != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to initialize display");
+            display_ = new NoDisplay();
+            return;
+        }
+
+        ESP_LOGI(TAG, "Turning display on");
+        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_, true));
+
+        display_ = new OledDisplay(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
+    }
+
+    void InitializeButtons() {
+         touch_button_.OnClick([this]() {
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting) {
+                EnterWifiConfigMode();
+                return;
+            }
+            if (!press_to_talk_tool_ || !press_to_talk_tool_->IsPressToTalkEnabled()) {
+                app.ToggleChatState();
+            }
+        });
+
+        touch_button_.OnPressDown([this]() {
+            if (power_save_timer_) {
+                power_save_timer_->WakeUp();
+            }
+           if (press_to_talk_tool_ && press_to_talk_tool_->IsPressToTalkEnabled()) {
+                Application::GetInstance().StartListening();
+            }
+        });
+        touch_button_.OnPressUp([this]() {
+            if (press_to_talk_tool_ && press_to_talk_tool_->IsPressToTalkEnabled()) {
+                Application::GetInstance().StopListening();
+            }
+        });
+    }
+
+    void InitializeTools() {
+        press_to_talk_tool_ = new PressToTalkMcpTool();
+        press_to_talk_tool_->Initialize();
+    }
+    
+public:
+    ESP32C3MicroBoard() : touch_button_(TOUCH_BUTTON_GPIO) {
+        InitializeDisplayI2c();
+        InitializeSsd1306Display();
+        InitializeButtons();
+        InitializePowerSaveTimer();
+        InitializeTools();
+    }
+
+    virtual AudioCodec* GetAudioCodec() override {
+
+        static NoAudioCodecSimplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT,
+            AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN);
+
+        return &audio_codec;
+    }
+
+    virtual Display* GetDisplay() override {
+        return display_;
+    }
+};
+
+DECLARE_BOARD(ESP32C3MicroBoard);
